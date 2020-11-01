32bcf4b047664d278da92ea4ce69ffa527dc581d
rlama7 <royalray7@gmail.com>
2020-10-31 21:01:43 -0700

    initial project setup

---
+++ src/main/java/com/ratnalama/springrestapi/SpringMvcRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.ratnalama.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcRestfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ratnalama/springrestapi/SpringMvcRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratnalama.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
