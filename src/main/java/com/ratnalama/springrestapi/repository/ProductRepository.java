9a3c410b477b4003b89ce773ac6ac1d55e0b7210
rlama7 <royalray7@gmail.com>
2020-11-02 17:58:20 -0800

    refactored ProductRepository class

--- src/main/java/com/ratnalama/springrestapi/repository/ProductRepository.java
+++ src/main/java/com/ratnalama/springrestapi/repository/ProductRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 /**
  * ProductRepository.java       --A program to represent a repository class for product entity.
+ *                                Using JpaRepository API we'll be able to perform CRUD opertions
+ *                                for Product entity with integer primary key type.
  * @author                        Ratna Lama
  * @version                       1.0
  * @since                         11/01/2020
